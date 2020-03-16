31c8e219bb9e23155c2a763430fe47621e029b0c
Darkangel <wtsegars@mac.com>
2020-03-16 17:26:32 -0400

    First version of constructor done

--- timestamp.cpp
+++ timestamp.cpp
@@ -1,2 +1,19 @@
 #include <cstdio>
 #include <cstring>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
+struct TimerClass {
+    TimerClass(time_t* timestamp) : timestamp(timestamp) {
+        std::chrono::system_clock::now();
+        printf("The current time is: %s\n", timestamp);
+    }
+
+    ~TimerClass() {
+        
+    }
+
+private:
+    time_t* timestamp;
+};
