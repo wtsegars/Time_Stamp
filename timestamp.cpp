d9ab7a248ed313e5dfeda5dd909582f3809533ef
Darkangel <wtsegars@mac.com>
2020-03-17 15:29:06 -0400

    updated time recording functionality

--- timestamp.cpp
+++ timestamp.cpp
@@ -5,15 +5,17 @@
 #include <ctime>
 
 struct TimerClass {
-    TimerClass(time_t* timestamp) : timestamp(timestamp) {
+    TimerClass(std::chrono::system_clock::time_point timestamp) : timestamp(timestamp) {
         std::chrono::system_clock::now();
         printf("The current time is: %s\n", timestamp);
     }
 
     ~TimerClass() {
-        
+        printf("Time since last construction: %s\n", timestamp);
     }
 
 private:
-    time_t* timestamp;
+    std::chrono::system_clock::time_point timestamp;
+    std::chrono::steady_clock::time_point starttime = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::time_point endtime = std::chrono::steady_clock::now();
 };
